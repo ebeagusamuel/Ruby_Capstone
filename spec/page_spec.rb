e864763a38771788c18970621786078322bdf434
ebeagusamuel <samuelebeagu@gmail.com>
2020-06-02 16:46:51 +0100

    Add a test case to check for the class of the elements of the array returned by the scrape method of the Scrapper class

---
+++ spec/page_spec.rb
@@ -0,0 +1,11 @@
+# require_relative '../lib/page.rb'
+# require_relative '../lib/scrapper.rb.rb'
+
+# describe Page do
+#   let(:scrapped) {Scrapper.new('https://coinmarketcap.com/')}
+#   let(:page) {Page.new(scrapped)}
+
+#   describe '#details' do
+    
+#   end
+# end
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/scrapper.rb'
+require_relative '../lib/page.rb'
 
 describe Scrapper do
   let(:scrapper_obj) { Scrapper.new('https://coinmarketcap.com/') }
@@ -10,6 +11,12 @@ describe Scrapper do
 
     it 'returns an array that is not empty' do
       expect(scrapper_obj.scrape.size).to be > 0
+    end
+
+    it 'expects the elememt of the array to be a Nokogori element' do
+      expected = Nokogiri::XML::Element
+      actual = scrapper_obj.scrape[0].class
+      expect(actual).to eql(expected)
     end
   end
 end
