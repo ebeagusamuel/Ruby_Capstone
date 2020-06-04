16ebba6d1f92b9ee7b8441e07c7d9ab14d68470b
ebeagusamuel <samuelebeagu@gmail.com>
2020-06-04 02:35:04 +0100

    Format the output to the console to be more readable

--- lib/page.rb
+++ lib/page.rb
@@ -8,7 +8,7 @@ class Page
   def details
     details_arr = []
     pages.each do |node|
-      details_hash = { name: node.css('td.cmc-table__cell--sort-by__name a').text,
+      details_hash = { Name: node.css('td.cmc-table__cell--sort-by__name a').text,
                        Market_cap: node.css('td.cmc-table__cell--sort-by__market-cap div').text,
                        Price: node.css('td.cmc-table__cell--sort-by__price a').text,
                        Volume: node.css('td.cmc-table__cell--sort-by__volume-24-h a').text,
